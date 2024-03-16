0767c37ac08ff6c58fe8e6362e0f6b5190539012
Jitpuwapat Mokkamakkul <jitpuwapat_mokkamakkul@brown.edu>
2024-03-15 21:29:01 -0400

    completed studio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,26 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  let cart_val = ["Nothing here just yet!"]
+  const [cart, setCart] = useState(cart_val)
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div className="Main">
+        <h1>Earth's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+        <div className="Grid">
+          {bakeryData.map((bakeryData, index) => ( // TODO: map bakeryData to BakeryItem components
+            <BakeryItem name = {bakeryData.name} description={bakeryData.description} price={bakeryData.price} image={bakeryData.image} setCart={setCart} cart={cart}/> // replace with BakeryItem component
+          ))}
+        </div>
+      </div>
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+      <div className="Cart">
+        <h2>My Cart</h2>
+        <div className="Items">
+          {cart.map((cart,index) => (<p>{cart}</p>))}
+        </div>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,61 @@
+import "../App.css";
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem(props) {
+
+    function addToCart() {
+        // for(let i = 0; i < length(props.cart); i ++) {}
+        if (props.cart[0] === "Nothing here just yet!") {
+            props.setCart(["1x ".concat(props.name), "Total: $".concat(props.price)])
+        } 
+        else {
+            const cart_val = props.cart.map((x) => x)
+            for(let i = 0; i < props.cart.length; i ++) {
+                var num = props.cart[i].match(/\d+/)[0] // "3"
+                var name = props.cart[i].match(/[\s](.*)/)[1]
+                var cost = props.cart[props.cart.length-1].match(/^\D*(\d+(?:\.\d+)?)/)[1]
+                if (name === props.name) {
+                    // console.log(num)
+                    var new_num = parseFloat(num) + 1
+                    // console.log(new_num)
+                    cart_val[i] = new_num.toString() + "x " + name
+
+                    var new_cost = parseFloat(cost) + props.price
+                    new_cost = Math.round(new_cost * 100) / 100
+
+                    console.log(props.price)
+                    cart_val[props.cart.length-1] = "Total: $".concat(new_cost)
+                    break
+                } 
+                else if (i === props.cart.length-1) {
+                    cart_val.splice(i, 0, "1x ".concat(props.name))
+
+                    var new_cost = parseFloat(cost) + props.price
+                    new_cost = Math.round(new_cost * 100) / 100
+                    
+                    cart_val[i+1] = "Total: $".concat(new_cost)
+                }
+            }
+            
+            props.setCart(cart_val)
+        }
+    }
+
+    return (
+        <div className="BakeryItem">
+            <img src={props.image} alt={props.name}/>
+
+            <div className="descriptors">
+                <div>
+                    <h4>{props.name}</h4>
+                    <p>{props.description}</p>
+                </div>
+                <div className="priceCart">
+                    <p>${props.price}</p>
+                    <button onClick={() => addToCart()}>Add to Cart</button>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
